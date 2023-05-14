81f666bc1e62a7b41cf4582ce1696eb4a1c095ff
Raymond Owhondah <raymond.owhondah@nhschocies.net>
2023-05-14 19:10:57 +0100

    feature to add area of cylinder

--- fun.py
+++ fun.py
@@ -1,3 +1,8 @@
 def area_of_rectangle(length, breath):
     area = length * breath
     return area
+
+
+def area_of_cylinder(radius, height, pi=3.1243):
+    cylinder_area = 2 * pi * radius * (radius + height)
+    return cylinder_area
